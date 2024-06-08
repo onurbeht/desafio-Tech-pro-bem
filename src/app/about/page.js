c9607628858b8b57a6ebc18e8a54d138069b0b83
onurbeht <brunnobatista25@gmail.com>
2024-06-08 13:17:48 -0300

    New: Navbar and Footer

---
+++ src/app/about/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="flex min-h-screen flex-col items-center justify-between pt-2">
+      About page
+    </section>
+  );
+};
+
+export default About;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ThemeProvider from "@/components/theme-provider";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,14 +14,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
-      >
-        <body className={inter.className}>{children}</body>
-      </ThemeProvider>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          // disableTransitionOnChange
+        >
+          <Navbar />
+          {children}
+          <Footer />
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
